6f060dcc0dcfd262dfa6f74dcbac00a1c8d176c2
zrahul15 <124668460+zrahul15@users.noreply.github.com>
2023-02-07 14:36:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace rahul
+{
+    class Paper
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Total no of Papers: ");
+            int papers = Convert.ToInt32(Console.ReadLine());
+
+
+            if (papers == 0)
+            {
+                Console.WriteLine("No Papers ");
+            }
+            else if (papers < 10)
+            {
+                Console.WriteLine("Less Papers");
+            }
+            else if (papers > 10)
+            {
+                Console.WriteLine("Ready");
+            }
+        }
+
+    }
+}
+
